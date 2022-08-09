07335fcbf3c86289ccd8c05db865967c556c0621
Paul Ccari <paulclindo@gmail.com>
2022-08-09 02:20:56 -0500

    setup docusaurus, fix and customize all mdx cardano/algorand docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,132 +1,145 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
-module.exports = {
-  title: 'Milkomeda Documentation',
-//  tagline: 'Getting started',
-  url: 'https://dcspark.github.io',
-  baseUrl: '/milkomeda-documentation/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-//  favicon: 'img/favicon.ico',
+const config = {
+  title: "Milkomeda Documentation",
+  //  tagline: 'Getting started',
+  url: "https://dcspark.github.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  // favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dcSpark', // Usually your GitHub org/user name.
-  projectName: 'milkomeda-documentation', // Usually your repo name.
+  organizationName: "dcSpark", // Usually your GitHub org/user name.
+  projectName: "milkomeda-documentation", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'ja'],
-	localeConfigs: {
+    defaultLocale: "en",
+    locales: ["en", "ja"],
+    localeConfigs: {
       en: {
-        label: 'English',
+        label: "English",
       },
       ja: {
-        label: 'Japanese',
+        label: "Japanese",
       },
     },
   },
+
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
+          routeBasePath: "/", // Serve the docs at the site's root
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-        //  editUrl:
-        //    'https://github.com/facebook/docusaurus/tree/main/packages/create-//docusaurus/templates/shared/',
+          // editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-         // editUrl:
-         //   'https://github.com/facebook/docusaurus/tree/main/packages/create-// docusaurus/templates/shared/',
-        },
+        blog: false,
+        // blog: {
+        //   showReadingTime: true,
+        //   // Please change this to your repo.
+        //   // Remove this to remove the "edit this page" links.
+        //   // editUrl: "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+        // },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
   ],
-
+  plugins: [
+    async function myPlugin(context, options) {
+      return {
+        name: "docusaurus-tailwindcss",
+        configurePostCss(postcssOptions) {
+          // Appends TailwindCSS and AutoPrefixer.
+          postcssOptions.plugins.push(require("tailwindcss"));
+          postcssOptions.plugins.push(require("autoprefixer"));
+          return postcssOptions;
+        },
+      };
+    },
+  ],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Milkomeda',
- //       logo: {
- //         alt: 'My Site Logo',
- //         src: 'img/logo.svg',
- //       },
+        title: "Milkomeda Getting started",
+        // logo: {
+        //   alt: "My Site Logo",
+        //   src: "img/logo.svg",
+        // },
         items: [
-          {
-            type: 'doc',
-            docId: 'getting-started/getting-started',
-            position: 'left',
-            label: 'Getting Started',
-          },
-     //     {to: '/blog', label: 'Blog', position: 'left'},
-     //     {
-      //      href: 'https://github.com/facebook/docusaurus',
-     //       label: 'GitHub',
-     //       position: 'right',
-     //     },
+          // {
+          //   type: "doc",
+          //   docId: "intro",
+          //   position: "left",
+          //   label: "Tutorial",
+          // },
+          // { to: "/blog", label: "Blog", position: "left" },
+          // {
+          //   href: "https://github.com/facebook/docusaurus",
+          //   label: "GitHub",
+          //   position: "right",
+          // },
         ],
       },
       footer: {
-        style: 'dark',
- //       links: [
- //         {
- //           title: 'Docs',
- //           items: [
- //             {
- //               label: 'Tutorial',
- //               to: '/docs/intro',
- //            },
- //           ],
- //         },
- //         {
- //           title: 'Community',
- //           items: [
- //             {
- //               label: 'Stack Overflow',
- //               href: 'https://stackoverflow.com/questions/tagged/docusaurus',
- //             },
-  //            {
-  //              label: 'Discord',
-  //              href: 'https://discordapp.com/invite/docusaurus',
-  //            },
-  //            {
-   //             label: 'Twitter',
-  //              href: 'https://twitter.com/docusaurus',
-  //            },
-  //          ],
-  //        },
-  //        {
-  //          title: 'More',
-  //          items: [
-  //            {
-  //              label: 'Blog',
-  //              to: '/blog',
-  //            },
-  //            {
-   //             label: 'GitHub',
-  //              href: 'https://github.com/facebook/docusaurus',
-  //            },
-  //          ],
-  //        },
-  //      ],
+        style: "dark",
+        // links: [
+        //   {
+        //     title: "Docs",
+        //     items: [
+        //       {
+        //         label: "Tutorial",
+        //         to: "/docs/intro",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: "Community",
+        //     items: [
+        //       {
+        //         label: "Stack Overflow",
+        //         href: "https://stackoverflow.com/questions/tagged/docusaurus",
+        //       },
+        //       {
+        //         label: "Discord",
+        //         href: "https://discordapp.com/invite/docusaurus",
+        //       },
+        //       {
+        //         label: "Twitter",
+        //         href: "https://twitter.com/docusaurus",
+        //       },
+        //     ],
+        //   },
+        //   {
+        //     title: "More",
+        //     items: [
+        //       {
+        //         label: "Blog",
+        //         to: "/blog",
+        //       },
+        //       {
+        //         label: "GitHub",
+        //         href: "https://github.com/facebook/docusaurus",
+        //       },
+        //     ],
+        //   },
+        // ],
         copyright: `Copyright © ${new Date().getFullYear()} Milkomeda Foundation. Built with Docusaurus.`,
       },
       prism: {
@@ -135,3 +148,5 @@ module.exports = {
       },
     }),
 };
+
+module.exports = config;
