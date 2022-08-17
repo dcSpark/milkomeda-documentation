be5c3c83f797f9d68bc8e13528996ff9a2238bdd
Paul Ccari <paulclindo@gmail.com>
2022-08-17 11:04:40 -0500

    misc fixes

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   baseUrl: "/milkomeda-documentation/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
-  favicon: "img/favicon.ico",
+  favicon: "img/favicon.svg",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
