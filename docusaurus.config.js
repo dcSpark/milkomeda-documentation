f908fd9f4a904b918b2d9de00381e43106984abf
Sebastien Guillemot <SebastienGllmt@gmail.com>
2022-09-08 23:59:49 +0900

    Add search bar (#7)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,23 @@ const config = {
     },
   },
 
+  themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        docsRouteBasePath: '/',
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      },
+    ],
+  ],
+
   presets: [
     [
       "classic",
