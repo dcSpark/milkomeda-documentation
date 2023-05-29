263507f0c1443f3a3d0571ecd52d401efdafd4c1
nhaga <nhaga5@gmail.com>
2023-05-30 00:27:12 +0100

    updated colors

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
     ({
       colorMode: {
         defaultMode: 'dark',
-        disableSwitch: false,
+        disableSwitch: true,
         respectPrefersColorScheme: false,
       },      
       navbar: {
